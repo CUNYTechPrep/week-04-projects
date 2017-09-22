c56f096ae61dec0052fc83f0481da98c1a959529
adam <ibrahimadam193@gmail.com>
2017-09-22 00:17:33 -0400

    Implement API.
    This was kind of a no-brainer.
    
    Changes to be committed:
          modified:   zip-api/app.js
          modified:   zip-api/package-lock.json

--- zip-api/app.js
+++ zip-api/app.js
@@ -15,14 +15,24 @@ app.get('/', (req, res) => {
 
 
 app.get('/zip/:zipcode', (req, res) => {
-  // fill in...
+	const zip = req.params.zipcode;
+	if (zipdb.byZip[zip] !== undefined) {
+		res.json(zipdb.byZip[zip]);
+	} else {
+		res.status(404).send("Not Found.");
+	}
 });
 
 
 app.get('/city/:cityname', (req, res) => {
-  // fill in...
+	// Why not do this? Why not just do the 
+	const city = req.params.cityname.toUpperCase();
+	if (zipdb.byCity[city] !== undefined) {
+		res.json(zipdb.byCity[city]);
+	} else {
+		res.status(404).send("Not Found.");
+	}
 });
-
 
 app.listen(PORT, () => {
   console.log(`zip-api is up and running on ${PORT}`);
