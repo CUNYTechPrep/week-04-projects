626762b34048cb7008c0f8652999f56137078b3b
Kayla Ford <Kayla.ford@mail.citytech.cuny.edu>
2017-09-27 09:06:39 -0400

    Finish Week 4 Homework

--- zip-api/app.js
+++ zip-api/app.js
@@ -16,11 +16,45 @@ app.get('/', (req, res) => {
 
 app.get('/zip/:zipcode', (req, res) => {
   // fill in...
+  values = {}
+  Cities =[];
+  const zip = req.params.zipcode;
+
+  for (var key in zipdb.byZip){
+    if(key == zip){
+      values =zipdb.byZip[key]
+};}
+for(i = 0; i <= values.length;i++)
+{
+  for(var key in values[i])
+  {
+    if(key == "City"){
+      Cities.push(values[i][key]);
+  
+  }
+  }
+}
+
+  res.send(Cities);
+  //res.send();
+
+
 });
 
 
 app.get('/city/:cityname', (req, res) => {
   // fill in...
+  values = [];
+  const city = req.params.cityname;
+
+  for(var key in zipdb.byCity)
+  {
+    if(key == city)
+    {
+        values.push(zipdb.byCity[key]);
+    }
+  }
+  res.send(values);
 });
 
 
