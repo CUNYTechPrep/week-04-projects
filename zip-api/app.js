f172d0d441307043691ead0a454fd7fb3f8a3e8b
ChristopherFerguson <cferguson575@yahoo.com>
2017-10-02 12:25:17 -0400

    app.js file completely works

--- zip-api/app.js
+++ zip-api/app.js
@@ -7,19 +7,48 @@ const PORT = process.env.PORT || 8000;
 
 
 // console.log(zipdb.byCity);
+app.get('/', (req, res) => {
+	
+  res.json({test: 'Hello World'});
+	
+});
+
 
 app.get('/zip/:zipcode', (req, res) => {
   // fill in...
   const zip = req.params.zipcode;
-  results = zipsdb.byZip[zip];
-  res.json(zip);
+  results = zipdb.byZip[zip];
+  res.json(results);
+  
+  //Next, we call the object constructor to iterate through the for loop
+  /*
+  Object.keys(zipdb.byZip).forEach(zipCodeElement => res.json(
+  {
+  	if(zip == zipCodeElement){
+  		res.json(zip);
+  	}
+  });
+  */
+
+  
 });
 
 app.get('/city/:cityname', (req, res) => {
   // fill in...
-  const city = req.params.cityname.toUpperCase;
-  results = zipsdb.byCity[city];
-  res.json(city);
+  const city = req.params.cityname;
+  results = zipdb.byCity[city];
+  records = results.toUpperCase; // I didn't use this line of code
+
+  res.json(results);
+/*
+  Object.keys(zipdb.byCity).forEach(cityElement => res.json(
+  {
+  	if(city == cityElement){
+  		res.json(zip);
+  	}
+  });
+  */
+  
 });
 
 
